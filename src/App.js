ee3801ac3868f5b4d4acc4580f7f3195cdff25db
NorbertSan <norbertsanpruch@gmail.com>
2020-02-26 19:51:06 +0100

    add initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import { size } from "./config";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div>app</div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Board/Board.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { size } from "./config";
+
+const Board = () => {
+  return {};
+};
+
+export default Board;
